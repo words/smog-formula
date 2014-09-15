01eeb43b3a2f2420f069d7801a353a61ba01d0b7
wooorm <tituswormer@gmail.com>
2014-09-15 11:12:56 +0200

    0.0.1

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * The constants as defined by the revised SMOG formula.
+ */
+
+var SENTENCE_SIZE = 30,
+    WEIGHT = 1.0430,
+    BASE = 3.1291;
+
+/**
+ * Get the grade level of a given value according to the SMOG formula.
+ * More information is available at WikiPedia:
+ *
+ *   http://en.wikipedia.org/wiki/SMOG
+ *
+ * @param {Object} counts
+ * @param {number} counts.sentence - Number of sentences.
+ * @param {number} counts.polysillabicWord - Number of polysillabic words
+ *   (three or more syllables).
+ * @return {number}
+ */
+
+function smog(counts) {
+    if (!counts || !counts.sentence) {
+        return NaN;
+    }
+
+    return WEIGHT * Math.sqrt(
+            (counts.polysillabicWord || 0) * (SENTENCE_SIZE / counts.sentence)
+        ) + BASE;
+}
+
+/**
+ * Export `smog`.
+ */
+
+module.exports = smog;
---
+++ spec/smog-formula.spec.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/**
+ * Module dependencies (smogFormula, assert).
+ */
+
+var smogFormula = require('..'),
+    assert = require('assert');
+
+/**
+ * Assert, but up to 6 digits.
+ */
+
+function roundAssert(a, b) {
+    assert(Math.round(a * 1000000) === Math.round(b * 1000000));
+}
+
+/**
+ * Unit tests.
+ */
+
+describe('smogFormula()', function () {
+    it('should be of type `function`', function () {
+        assert(typeof smogFormula === 'function');
+    });
+
+    it('should work', function () {
+        var result;
+
+        /**
+         * Return NaN when an invalid value is given.
+         */
+
+        result = smogFormula();
+
+        assert(result !== result);
+
+        /**
+         * For a sentence without polysillabic words.
+         */
+
+        roundAssert(smogFormula({
+            'sentence' : 1
+        }), 3.1291);
+
+        /**
+         * For “The Australian platypus is seemingly a hybrid of
+         * a mammal and reptilian creature.”
+         *
+         * Sentences: 1
+         * Polysillabic: 4 (Australian, platypus, seemingly, reptilian)
+         */
+
+        roundAssert(smogFormula({
+            'sentence' : 1,
+            'polysillabicWord' : 4
+        }), 14.554593);
+    });
+});
