b4065704e955c082f39d85df2ff87fb4a248544d
Titus Wormer <tituswormer@gmail.com>
2021-04-06 10:21:53 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+/**
+ * @typedef {Object} SmogFormulaCounts
+ * @property {number} sentence
+ * @property {number} [polysillabicWord]
+ */
+
 var sentenceSize = 30
 var weight = 1.043
 var base = 3.1291
 
-// Get the grade level of a given value according to the SMOG formula.
-// More information is available at WikiPedia:
-// <https://en.wikipedia.org/wiki/SMOG>
+/**
+ * Get the grade level of a given value according to the SMOG formula. More information is available at WikiPedia: <https://en.wikipedia.org/wiki/SMOG>.
+ *
+ * @param {SmogFormulaCounts} counts
+ * @returns {number}
+ */
 export function smogFormula(counts) {
   if (!counts || !counts.sentence) {
     return Number.NaN
--- test.js
+++ test.js
@@ -2,12 +2,17 @@ import test from 'tape'
 import {smogFormula} from './index.js'
 
 test('smogFormula', function (t) {
+  // @ts-ignore runtime.
   t.ok(Number.isNaN(smogFormula()), 'NaN when an invalid value is given')
   t.equal(round(smogFormula({sentence: 1})), 3.1291)
   t.equal(round(smogFormula({sentence: 1, polysillabicWord: 4})), 14.554593)
   t.end()
 })
 
+/**
+ * @param {number} value
+ * @returns {number}
+ */
 function round(value) {
   return Math.round(value * 1e6) / 1e6
 }
