444c56aa4b82818f0f68e6bb7241100eee13f08b
Titus Wormer <tituswormer@gmail.com>
2022-11-01 15:39:05 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,12 +1,15 @@
-import test from 'tape'
+import assert from 'node:assert'
+import test from 'node:test'
 import {smogFormula} from './index.js'
 
-test('smogFormula', function (t) {
-  // @ts-ignore runtime.
-  t.ok(Number.isNaN(smogFormula()), 'NaN when an invalid value is given')
-  t.equal(round(smogFormula({sentence: 1})), 3.1291)
-  t.equal(round(smogFormula({sentence: 1, polysillabicWord: 4})), 14.554_593)
-  t.end()
+test('smogFormula', function () {
+  // @ts-expect-error runtime.
+  assert.ok(Number.isNaN(smogFormula()), 'NaN when an invalid value is given')
+  assert.equal(round(smogFormula({sentence: 1})), 3.1291)
+  assert.equal(
+    round(smogFormula({sentence: 1, polysillabicWord: 4})),
+    14.554_593
+  )
 })
 
 /**
